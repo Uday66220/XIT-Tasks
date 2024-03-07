9b86769327972d5b80a9733b66dc66211e98e604
Uday <uday66220@gmail.com>
2024-03-07 20:54:18 +0530

    second commit

--- Task-8/MaxProduct.java
+++ Task-8/MaxProduct.java
@@ -1,3 +1,33 @@
+/*
+ * Given an integer n, break into  the sum of k positive integers, and return the maximum product of those integers.
+Input:
+Integer n
+Desired Output:
+Maximum product of numbers whose sum is n.
+Example 1:
+n=5;
+5 can be written as sum of following numbers.
+Possible Sums:
+1.            1+1+1+1+1,
+2.            1+4,
+3.            1+1+3,
+4.            1+1+1+2,
+5.            2+3,
+6.            1+2+2.....etc
+Product of these integers:
+1.            1*1*1*1*1=1,
+2.            1*4=4,
+3.            1*1*3=3,
+4.            1*1*1*2=2
+5.            2*3=6,
+6.            1*2*2=4.....etc
+Output returned should be maximum product which in the above case is 6.
+Example 2:
+n=10;
+Desired Output : 36
+Integers : 3,3,4 or 3,3,2,2
+ */
+
 import java.util.*;
 
 class MaxProduct {
